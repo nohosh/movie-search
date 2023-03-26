14d0d92eb1d02cfbe958744589902997affbf2fe
Sanchit <43117486+nohosh@users.noreply.github.com>
2023-03-26 23:36:36 +0530

    Add useMovieSearch hook, API error cases handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, {  useState } from 'react';
 import './App.css';
 import Search from './components/Search';
-import { KEY } from './constants';
+import useMovieSearch from './hooks/useMovieSearch';
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [movies, setMovies] = useState<any[]>([]);
-  const pageNumber = '1';
-  useEffect(()=>{
-    fetch(`https://www.omdbapi.com/?s=${search}&page=${pageNumber}&apikey=${KEY}`).then(res=>res.json())
-    .then(res=>setMovies(res.Search));
-  },[search]);
+  const [search, setSearch] = useState('');  
+  const [pageNo, setPageNo] = useState(1); 
+  const { movies, hasMore, loading, errMsg } = useMovieSearch(
+    search,
+    `${pageNo}`
+  );
+
   return (
     <div className="App">
     <Search  setSearch={setSearch}/>
@@ -20,6 +19,7 @@ function App() {
       return <li>{movie.Title}</li>
     })}
     </ul>
+    <button onClick={()=>setPageNo(pageNo+1)}>+1</button>
     </div>
   );
 }
---
+++ src/hooks/useMovieSearch.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { KEY } from "../constants";
+
+const movieCache = new Map();
+export default function useMovieSearch(query: string, pageNumber: string) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [movies, setMovies] = useState<any[]>([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    setMovies([]);
+  }, [query]);
+
+  useEffect(() => {
+    if (query) {
+      if (pageNumber === "0" && movieCache.has(query)) {
+        setMovies(movieCache.get(query));
+      } else {
+        setLoading(true);
+        setError(false);
+        fetch(
+          `https://www.omdbapi.com/?s=${query}&page=${pageNumber}&apikey=${KEY}`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.Response === "True") {
+              if (!movieCache.has(query)) {
+                movieCache.set(query, res.Search);
+              }
+              setMovies((prev: any) => {
+                return [...prev, ...res.Search];
+              });
+              setHasMore(
+                Math.ceil(Number(res.totalResults) / 10) > Number(pageNumber)
+              );
+            } else if (res.Response === "False") {
+              setError(true);
+              setErrMsg(res.Error);
+            }
+          })
+          .catch((e) => {
+            setError(true);
+            setErrMsg(e);
+          })
+          .finally(() => setLoading(false));
+      }
+    }
+  }, [query, pageNumber]);
+
+  return { loading, error, errMsg, movies, hasMore };
+}
