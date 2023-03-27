ec1a8ba10bdd88732ac79ba116d5040020a2fa8e
Sanchit <43117486+nohosh@users.noreply.github.com>
2023-03-27 13:25:58 +0530

    Add snackbar error msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React, {  useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import './App.css';
 import Search from './components/Search';
+import Snackbar from './components/Snackbar';
 import useMovieSearch from './hooks/useMovieSearch';
 
 function App() {
   const [search, setSearch] = useState('');  
   const [pageNo, setPageNo] = useState(1); 
+  const [snackBar, setSnackBar] = useState(false);
   const { movies, hasMore, loading, errMsg } = useMovieSearch(
     search,
     `${pageNo}`
   );
+
+  useEffect(()=>{    
+    if(errMsg.length>0)setSnackBar(true);
+  }, [errMsg]);
 
   return (
     <div className="App">
@@ -20,7 +26,13 @@ function App() {
     })}
     </ul>
     <button onClick={()=>setPageNo(pageNo+1)}>+1</button>
+    <Snackbar
+        showSnackbar={snackBar}
+        message={errMsg}
+        onClose={() => setSnackBar(false)}
+      />
     </div>
+
   );
 }
 
---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+type Props = {
+  showSnackbar: boolean;
+  message: string;
+  onClose: () => void;
+};
+const Snackbar: React.FC<Props> = ({
+  showSnackbar,
+  message,
+  onClose,
+}) => {  
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      onClose();
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  return !showSnackbar ? null : (
+    <div className="snackbar" onClick={(e) => e.stopPropagation()}>
+      {message}
+    </div>
+  );
+};
+
+export default Snackbar;
